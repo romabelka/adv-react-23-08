4353fa7b42df1f83c0c05fb83babe6ed8b517907
Alexander Koltun <alexander.koltun@vasco.com>
2018-08-28 18:41:47 +0200

    Add people duck and reducer

---
+++ admin/src/ducks/people.js
@@ -0,0 +1,50 @@
+import { appName } from '../config'
+import { Record, List } from 'immutable'
+// import firebase from 'firebase/app'
+
+/**
+ * Constants
+ * */
+export const moduleName = 'people'
+const prefix = `${appName}/${moduleName}`
+
+export const CREATE_PERSON = `${prefix}/CREATE_PERSON`
+
+/**
+ * Reducer
+ * */
+export const ReducerList = List
+
+const PersonRecord = Record({
+  firstName: null,
+  lastName: null,
+  email: null
+})
+export default function reducer(state = new ReducerList(), action) {
+  const { type, payload } = action
+
+  switch (type) {
+    case CREATE_PERSON:
+      return state.push(new PersonRecord(payload))
+
+    default:
+      return state
+  }
+}
+
+/**
+ * Selectors
+ * */
+
+/**
+ * Action Creators
+ * */
+
+export function createPerson(firstName, lastName, email) {
+  return (dispatch) => {
+    dispatch({
+      type: CREATE_PERSON,
+      payload: { firstName, lastName, email }
+    })
+  }
+}
