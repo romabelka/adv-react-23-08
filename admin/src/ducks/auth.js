52479333df4d764869269d5c941445731e062a19
Alexey Germanov <lexx.vl@gmail.com>
2018-08-30 12:07:10 +0300

    Bring back onAuthStateChanged

--- admin/src/ducks/auth.js
+++ admin/src/ducks/auth.js
@@ -66,3 +66,10 @@ export function signIn(email, password) {
       )
   }
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+  window.store.dispatch({
+    type: SIGN_IN_SUCCESS,
+    payload: { user }
+  })
+})
