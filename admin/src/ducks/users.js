d31c1403dd75f0784437d80bd2b9732c108d6c7f
Victor Zagorodny <post.vittorius@gmail.com>
2018-08-30 10:31:34 +0300

    chore: remove test data for users map

--- admin/src/ducks/users.js
+++ admin/src/ducks/users.js
@@ -15,23 +15,8 @@ const ADD_USER = `${prefix}/ADD_USER`
 /// Reducer
 ///
 
-// const UsersOrderedMap = new OrderedMap()
-
 const UserRecord = Record({ firstName: null, lastName: null, email: null })
-
-// TODO: remove in favor of real initial state (empty map)
-const UsersOrderedMap = new OrderedMap({
-  'foo@example.com': new UserRecord({
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'foo@example.com'
-  }),
-  'bar@example.com': new UserRecord({
-    firstName: 'Jane',
-    lastName: 'Bar',
-    email: 'bar@example.com'
-  })
-})
+const UsersOrderedMap = new OrderedMap()
 
 export default function reducer(state = UsersOrderedMap, action) {
   const { type } = action
