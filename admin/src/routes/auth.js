6cd4fa0b4dddc9830fdaac92f2d7a1cd66a6a7c3
Victor Zagorodny <post.vittorius@gmail.com>
2018-08-30 08:10:41 +0300

    feat: redirect to admin page if authorized

--- admin/src/routes/auth.js
+++ admin/src/routes/auth.js
@@ -3,20 +3,30 @@ import { NavLink, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import SignInForm from '../components/auth/sign-in-form'
 import SignUpForm from '../components/auth/sign-up-form'
-import { signUp, signIn } from '../ducks/auth'
+import { moduleName, signUp, signIn } from '../ducks/auth'
+import { Redirect } from 'react-router-dom'
 
 class AuthRoute extends Component {
   static propTypes = {}
 
+  // TODO: extract into HOC
+  get authorized() {
+    return !!this.props.currentUser
+  }
+
   render() {
-    return (
-      <div>
-        <h2>Auth Page</h2>
-        {this.navMenu}
-        <Route path="/auth/sign-in" render={this.signInForm} />
-        <Route path="/auth/sign-up" render={this.signUpForm} />
-      </div>
-    )
+    if (this.authorized) {
+      return <Redirect to="/admin" />
+    } else {
+      return (
+        <div>
+          <h2>Auth Page</h2>
+          {this.navMenu}
+          <Route path="/auth/sign-in" render={this.signInForm} />
+          <Route path="/auth/sign-up" render={this.signUpForm} />
+        </div>
+      )
+    }
   }
 
   get navMenu() {
@@ -44,6 +54,6 @@ class AuthRoute extends Component {
 }
 
 export default connect(
-  null,
+  ({ [moduleName]: auth }) => ({ currentUser: auth.user }),
   { signUp, signIn }
 )(AuthRoute)
