753f326a0f598b2a5eaedb238706905ade46286b
Victor Zagorodny <post.vittorius@gmail.com>
2018-08-30 08:08:36 +0300

    chore: remove console.log

--- admin/src/components/common/error.js
+++ admin/src/components/common/error.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { moduleName as errorModule } from '../../ducks/error'
 
 function Error({ error }) {
-  console.log('error', error)
   return error ? <p style={{ color: 'red' }}>{error.message}</p> : null
 }
 
