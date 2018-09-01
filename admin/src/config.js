f5a50ba04124a8b574c30acd797dc549c8cf3e6e
Victor Zagorodny <post.vittorius@gmail.com>
2018-09-01 19:54:59 +0300

    feat: my FB config and firestore setup

--- admin/src/config.js
+++ admin/src/config.js
@@ -1,16 +1,20 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import 'firebase/database'
+import 'firebase/firestore'
 
 export const appName = 'adv-react-23-08'
 
 const fbConfig = {
-  apiKey: 'AIzaSyAlWpSCiLQOATDM2O869VEYBfT3kuDuxc0',
-  authDomain: `${appName}.firebaseapp.com`,
-  databaseURL: `https://${appName}.firebaseio.com`,
-  projectId: appName,
-  storageBucket: `${appName}.appspot.com`,
-  messagingSenderId: '842367541781'
+  apiKey: 'AIzaSyCgw2wKSWCuWBy3ZSqvn5O3Q7mq1wf21ug',
+  authDomain: 'adv-react-23-08-vittorius.firebaseapp.com',
+  databaseURL: 'https://adv-react-23-08-vittorius.firebaseio.com',
+  projectId: 'adv-react-23-08-vittorius',
+  storageBucket: 'adv-react-23-08-vittorius.appspot.com',
+  messagingSenderId: '999406358227'
 }
 
 firebase.initializeApp(fbConfig)
+
+export const db = firebase.firestore()
+const settings = { timestampsInSnapshots: true }
+db.settings(settings)
