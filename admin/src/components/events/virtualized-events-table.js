efc93632c4dc9dfec3852da84958623a66e23081
Alexey Germanov <lexx.vl@gmail.com>
2018-09-06 13:33:00 +0300

    Remove autosizer

--- admin/src/components/events/virtualized-events-table.js
+++ admin/src/components/events/virtualized-events-table.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Table, Column, AutoSizer, InfiniteLoader } from 'react-virtualized'
+import { Table, Column, InfiniteLoader } from 'react-virtualized'
 import 'react-virtualized/styles.css'
 import {
   fetchAllEvents,
@@ -10,7 +10,6 @@ import {
   loadingSelector,
   toggleSelect as handleSelect
 } from '../../ducks/events'
-import Loader from '../common/loader'
 
 export class EventsTable extends Component {
   static propTypes = {}
@@ -20,39 +19,33 @@ export class EventsTable extends Component {
   }
 
   render() {
-    const { loaded, minWidth, events } = this.props
+    const { loaded, events } = this.props
 
     return (
-      <div className="vt-container" style={{ outline: '1px dotted red' }}>
-        <InfiniteLoader
-          isRowLoaded={this.isRowLoaded}
-          loadMoreRows={this.loadMoreRows}
-          rowCount={loaded ? events.lenght : events.length + 1}
-        >
-          {({ onRowsRendered, registerChild }) => (
-            <AutoSizer disableHeight>
-              {({ width }) => (
-                <Table
-                  ref={registerChild}
-                  rowHeight={50}
-                  headerHeight={80}
-                  width={minWidth ? Math.max(minWidth, width) : width}
-                  height={400}
-                  rowGetter={this.rowGetter}
-                  rowCount={events.length}
-                  overscanRowCount={10}
-                  onRowClick={this.handleRowClick}
-                  onRowsRendered={onRowsRendered}
-                >
-                  <Column dataKey="title" width={200} label="Title" />
-                  <Column dataKey="when" width={100} label="Date" />
-                  <Column dataKey="where" width={200} label="Place" />
-                </Table>
-              )}
-            </AutoSizer>
-          )}
-        </InfiniteLoader>
-      </div>
+      <InfiniteLoader
+        isRowLoaded={this.isRowLoaded}
+        loadMoreRows={this.loadMoreRows}
+        rowCount={loaded ? events.lenght : events.length + 1}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <Table
+            ref={registerChild}
+            rowHeight={50}
+            headerHeight={80}
+            width={500}
+            height={400}
+            rowGetter={this.rowGetter}
+            rowCount={events.length}
+            overscanRowCount={20}
+            onRowClick={this.handleRowClick}
+            onRowsRendered={onRowsRendered}
+          >
+            <Column dataKey="title" width={200} label="Title" />
+            <Column dataKey="when" width={100} label="Date" />
+            <Column dataKey="where" width={200} label="Place" />
+          </Table>
+        )}
+      </InfiniteLoader>
     )
   }
 
