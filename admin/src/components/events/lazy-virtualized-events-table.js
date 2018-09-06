93a86df16421b139129a0a33e15f6b6221a3578b
Alexey Germanov <lexx.vl@gmail.com>
2018-09-06 14:47:52 +0300

    Move lazy to lazy-virualized-events-table.js

---
+++ admin/src/components/events/lazy-virtualized-events-table.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Table, Column, InfiniteLoader } from 'react-virtualized'
+import 'react-virtualized/styles.css'
+import {
+  fetchAllEvents,
+  fetchLazyEvents,
+  eventListSelector,
+  loadedSelector,
+  loadingSelector,
+  toggleSelect as handleSelect
+} from '../../ducks/events'
+
+export class EventsTable extends Component {
+  static propTypes = {}
+
+  componentDidMount() {
+    this.props.fetchLazyEvents()
+  }
+
+  render() {
+    const { loaded, events } = this.props
+
+    return (
+      <InfiniteLoader
+        isRowLoaded={this.isRowLoaded}
+        loadMoreRows={this.loadMoreRows}
+        rowCount={loaded ? events.lenght : events.length + 1}
+      >
+        {({ onRowsRendered, registerChild }) => (
+          <Table
+            ref={registerChild}
+            rowHeight={50}
+            headerHeight={80}
+            width={500}
+            height={400}
+            rowGetter={this.rowGetter}
+            rowCount={events.length}
+            overscanRowCount={20}
+            onRowClick={this.handleRowClick}
+            onRowsRendered={onRowsRendered}
+          >
+            <Column dataKey="title" width={200} label="Title" />
+            <Column dataKey="when" width={100} label="Date" />
+            <Column dataKey="where" width={200} label="Place" />
+          </Table>
+        )}
+      </InfiniteLoader>
+    )
+  }
+
+  handleRowClick = ({ rowData }) => this.props.handleSelect(rowData.id)
+
+  rowGetter = ({ index }) => this.props.events[index]
+
+  isRowLoaded = ({ index }) => index < this.props.events.length
+
+  loadMoreRows = () => {
+    this.props.fetchLazyEvents()
+  }
+}
+
+export default connect(
+  (state) => ({
+    events: eventListSelector(state),
+    loading: loadingSelector(state),
+    loaded: loadedSelector(state)
+  }),
+  { fetchLazyEvents, handleSelect }
+)(EventsTable)
--- admin/src/components/events/virtualized-events-table.js
+++ admin/src/components/events/virtualized-events-table.js
@@ -1,63 +1,46 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Table, Column, InfiniteLoader } from 'react-virtualized'
-import 'react-virtualized/styles.css'
+import { Table, Column } from 'react-virtualized'
 import {
   fetchAllEvents,
-  fetchLazyEvents,
   eventListSelector,
   loadedSelector,
   loadingSelector,
   toggleSelect as handleSelect
 } from '../../ducks/events'
+import Loader from '../common/loader'
+import 'react-virtualized/styles.css'
 
 export class EventsTable extends Component {
   static propTypes = {}
 
   componentDidMount() {
-    this.props.fetchLazyEvents()
+    this.props.fetchAllEvents()
   }
 
   render() {
-    const { loaded, events } = this.props
-
+    if (this.props.loading && !this.props.loaded) return <Loader />
     return (
-      <InfiniteLoader
-        isRowLoaded={this.isRowLoaded}
-        loadMoreRows={this.loadMoreRows}
-        rowCount={loaded ? events.lenght : events.length + 1}
+      <Table
+        rowHeight={50}
+        headerHeight={80}
+        width={500}
+        height={400}
+        rowGetter={this.rowGetter}
+        rowCount={this.props.events.length}
+        overscanRowCount={0}
+        onRowClick={this.handleRowClick}
       >
-        {({ onRowsRendered, registerChild }) => (
-          <Table
-            ref={registerChild}
-            rowHeight={50}
-            headerHeight={80}
-            width={500}
-            height={400}
-            rowGetter={this.rowGetter}
-            rowCount={events.length}
-            overscanRowCount={20}
-            onRowClick={this.handleRowClick}
-            onRowsRendered={onRowsRendered}
-          >
-            <Column dataKey="title" width={200} label="Title" />
-            <Column dataKey="when" width={100} label="Date" />
-            <Column dataKey="where" width={200} label="Place" />
-          </Table>
-        )}
-      </InfiniteLoader>
+        <Column dataKey="title" width={200} label="Title" />
+        <Column dataKey="when" width={100} label="Date" />
+        <Column dataKey="where" width={200} label="Place" />
+      </Table>
     )
   }
 
   handleRowClick = ({ rowData }) => this.props.handleSelect(rowData.id)
 
   rowGetter = ({ index }) => this.props.events[index]
-
-  isRowLoaded = ({ index }) => index < this.props.events.length
-
-  loadMoreRows = () => {
-    this.props.fetchLazyEvents()
-  }
 }
 
 export default connect(
@@ -66,5 +49,5 @@ export default connect(
     loading: loadingSelector(state),
     loaded: loadedSelector(state)
   }),
-  { fetchAllEvents, fetchLazyEvents, handleSelect }
+  { fetchAllEvents, handleSelect }
 )(EventsTable)
