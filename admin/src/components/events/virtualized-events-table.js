a5c59d771160211ba89e29ce42942ecca136ce89
demchuk-alex <ademchuk@provectus.com>
2018-09-06 14:56:47 +0300

    Added infinit loader as an implementation of lazy loading

--- admin/src/components/events/virtualized-events-table.js
+++ admin/src/components/events/virtualized-events-table.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Table, Column } from 'react-virtualized'
+import { Table, Column, InfiniteLoader } from 'react-virtualized'
 import {
   fetchAllEvents,
   eventListSelector,
   loadedSelector,
   loadingSelector,
-  toggleSelect as handleSelect
+  toggleSelect as handleSelect,
+  getChunck,
+  selectEventsCount
 } from '../../ducks/events'
 import Loader from '../common/loader'
 import 'react-virtualized/styles.css'
@@ -18,23 +20,45 @@ export class EventsTable extends Component {
     this.props.fetchAllEvents()
   }
 
+  isRowLoaded = ({ index }) => {
+    const val = this.props.events[index]
+    return val && val.id
+  }
+
+  loadMoreRows = ({ startIndex, stopIndex }) => {
+    return new Promise((resolve) => {
+      this.props.getChunck({ startIndex, stopIndex })
+      resolve()
+    })
+  }
+
   render() {
     if (this.props.loading && !this.props.loaded) return <Loader />
     return (
-      <Table
-        rowHeight={50}
-        headerHeight={80}
-        width={500}
-        height={400}
-        rowGetter={this.rowGetter}
-        rowCount={this.props.events.length}
-        overscanRowCount={0}
-        onRowClick={this.handleRowClick}
+      <InfiniteLoader
+        isRowLoaded={this.isRowLoaded}
+        loadMoreRows={this.loadMoreRows}
+        rowCount={this.props.recordCount}
       >
-        <Column dataKey="title" width={200} label="Title" />
-        <Column dataKey="when" width={100} label="Date" />
-        <Column dataKey="where" width={200} label="Place" />
-      </Table>
+        {({ onRowsRendered, registerChild }) => (
+          <Table
+            rowHeight={50}
+            headerHeight={80}
+            width={500}
+            height={400}
+            rowGetter={this.rowGetter}
+            rowCount={this.props.events.length}
+            overscanRowCount={0}
+            onRowClick={this.handleRowClick}
+            onRowsRendered={onRowsRendered}
+            ref={registerChild}
+          >
+            <Column dataKey="title" width={200} label="Title" />
+            <Column dataKey="when" width={100} label="Date" />
+            <Column dataKey="where" width={200} label="Place" />
+          </Table>
+        )}
+      </InfiniteLoader>
     )
   }
 
@@ -47,7 +71,8 @@ export default connect(
   (state) => ({
     events: eventListSelector(state),
     loading: loadingSelector(state),
-    loaded: loadedSelector(state)
+    loaded: loadedSelector(state),
+    recordCount: selectEventsCount(state)
   }),
-  { fetchAllEvents, handleSelect }
+  { fetchAllEvents, handleSelect, getChunck }
 )(EventsTable)
--- admin/src/ducks/events.js
+++ admin/src/ducks/events.js
@@ -16,6 +16,8 @@ export const FETCH_ALL_START = `${prefix}/FETCH_ALL_START`
 export const FETCH_ALL_SUCCESS = `${prefix}/FETCH_ALL_SUCCESS`
 
 export const TOGGLE_SELECT = `${prefix}/TOGGLE_SELECT`
+export const FETCH_CHUNCK = `${prefix}/FETCH_CHUNCK`
+export const FETCH_CHUNCK_SUCCESSFULL = `${prefix}/FETCH_CHUNCK_SUCCESSFULL`
 
 /**
  * Reducer
@@ -24,7 +26,8 @@ export const ReducerRecord = Record({
   loading: false,
   loaded: false,
   selected: new OrderedSet([]),
-  entities: new OrderedMap()
+  entities: new OrderedMap([]),
+  count: 10
 })
 
 export const EventRecord = Record({
@@ -45,10 +48,26 @@ export default function reducer(state = new ReducerRecord(), action) {
       return state.set('loading', true)
 
     case FETCH_ALL_SUCCESS:
+      const res = fbToEntities(payload.events, EventRecord)
       return state
         .set('loading', false)
         .set('loaded', true)
-        .set('entities', fbToEntities(payload, EventRecord))
+        .set('entities', fbToEntities(payload.events, EventRecord))
+        .set('count', payload.count)
+
+    case FETCH_CHUNCK_SUCCESSFULL:
+      let events
+      let count
+      if (payload.events) {
+        events = state
+          .get('entities')
+          .merge(fbToEntities(payload.events, EventRecord))
+        count = payload.count
+      } else {
+        events = state.get('entities')
+        count = state.get('entities').toArray().length
+      }
+      return state.set('entities', events).set('count', count)
 
     case TOGGLE_SELECT:
       return state.update(
@@ -94,6 +113,11 @@ export const selectedEventsListSelector = createSelector(
   entitiesSelector,
   (ids, entities) => ids.map((id) => entities.get(id))
 )
+
+export const selectEventsCount = createSelector(
+  stateSelector,
+  (state) => state.count
+)
 /**
  * Action Creators
  * */
@@ -111,28 +135,69 @@ export function toggleSelect(id) {
   }
 }
 
+export function getChunck({ startIndex, stopIndex }) {
+  return {
+    type: FETCH_CHUNCK,
+    payload: { startIndex, stopIndex }
+  }
+}
+
 /**
  * Sagas
  * */
 
+export function* fetchChunck({ payload }) {
+  const { count } = yield select(stateSelector)
+  const start = payload.startIndex - 1
+  const events = yield select(eventListSelector)
+  const startItem = events[payload.startIndex - 1]
+  const howManyToLoad = payload.stopIndex - start
+  const ref = firebase
+    .database()
+    .ref('events')
+    .orderByKey()
+    .limitToFirst(howManyToLoad)
+    .startAt(startItem.id)
+
+  const snapshot = yield call([ref, ref.once], 'value')
+
+  const payload0 = snapshot.val()
+  yield put({
+    type: FETCH_CHUNCK_SUCCESSFULL,
+    payload: {
+      events: payload0,
+      count: count + howManyToLoad
+    }
+  })
+}
+
 export function* fetchAllSaga() {
-  const { loading, loaded } = yield select(stateSelector)
+  const { loading, loaded, count } = yield select(stateSelector)
   if (loading || loaded) return
 
-  const ref = firebase.database().ref('events')
-
+  const ref = firebase
+    .database()
+    .ref('events')
+    .orderByKey()
+    .limitToFirst(10)
   yield put({
     type: FETCH_ALL_START
   })
 
   const snapshot = yield call([ref, ref.once], 'value')
-
+  const payload = snapshot.val()
   yield put({
     type: FETCH_ALL_SUCCESS,
-    payload: snapshot.val()
+    payload: {
+      events: payload,
+      count: count + 10
+    }
   })
 }
 
 export function* saga() {
-  yield all([takeEvery(FETCH_ALL_REQUEST, fetchAllSaga)])
+  yield all([
+    takeEvery(FETCH_ALL_REQUEST, fetchAllSaga),
+    takeEvery(FETCH_CHUNCK, fetchChunck)
+  ])
 }
