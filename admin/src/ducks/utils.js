9b388cda723bd0c6e8486a61af9433ae7f94f704
Victor Zagorodny <post.vittorius@gmail.com>
2018-09-09 11:44:20 +0300

    fix: protect fbToEntities from missing ref condition

--- admin/src/ducks/utils.js
+++ admin/src/ducks/utils.js
@@ -5,10 +5,14 @@ export function generateId() {
 }
 
 export function fbToEntities(values, DataRecord) {
-  return new OrderedMap(
-    Object.entries(values).map(([id, value]) => [
-      id,
-      new DataRecord({ id, ...value })
-    ])
-  )
+  if (values) {
+    return new OrderedMap(
+      Object.entries(values).map(([id, value]) => [
+        id,
+        new DataRecord({ id, ...value })
+      ])
+    )
+  } else {
+    return new OrderedMap()
+  }
 }
