ff94475bce025eff6122d3ec49160af78a275c9d
Leisan Kazberova <leisan.kh@gmail.com>
2018-09-13 16:28:03 +0300

    HT5.1

--- admin/src/components/events/selected-events.js
+++ admin/src/components/events/selected-events.js
@@ -3,25 +3,54 @@ import { connect } from 'react-redux'
 import { List } from 'react-virtualized'
 import { selectedEventsListSelector } from '../../ducks/events'
 import SelectedEventCard from './selected-event-card'
+import { spring, TransitionMotion, presets } from 'react-motion'
 
 class SelectedEvents extends Component {
   static propTypes = {}
 
   render() {
+    const { events } = this.props
     return (
-      <List
-        width={400}
-        height={300}
-        rowCount={this.props.events.length}
-        rowHeight={100}
-        rowRenderer={this.rowRenderer}
-      />
+      <TransitionMotion
+        willLeave={this.willLeave}
+        willEnter={this.willEnter}
+        defaultStyles={events.map((item) => ({
+          key: item.id,
+          style: { height: 0, opacity: 0 },
+          data: item
+        }))}
+        styles={events.map((item) => ({
+          key: item.id,
+          data: item,
+          style: {
+            width: spring(400),
+            height: spring(100),
+            opacity: spring(1)
+          }
+        }))}
+      >
+        {(styles) => (
+          <List
+            width={400}
+            height={300}
+            rowCount={styles.length}
+            rowHeight={100}
+            rowRenderer={this.rowRenderer(styles)}
+          />
+        )}
+      </TransitionMotion>
     )
   }
+  willLeave = () => ({
+    width: spring(0),
+    height: spring(0),
+    opacity: spring(0)
+  })
+  willEnter = () => ({ width: 0, height: 0, opacity: 1 })
 
-  rowRenderer = ({ index, key, style }) => (
-    <div key={key} style={style}>
-      <SelectedEventCard event={this.props.events[index]} />
+  rowRenderer = (styles) => ({ index, key, style }) => (
+    <div key={key} style={{ ...style, ...styles[index].style }}>
+      <SelectedEventCard event={styles[index].data} />
     </div>
   )
 }
--- admin/src/config.js
+++ admin/src/config.js
@@ -2,15 +2,15 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
 
-export const appName = 'adv-react-23-08'
+export const appName = 'adv-react-53b1c'
 
 const fbConfig = {
-  apiKey: 'AIzaSyAlWpSCiLQOATDM2O869VEYBfT3kuDuxc0',
+  apiKey: 'AIzaSyBMgo5tCsH2-DZqULliABpR0qxgXmVecdc',
   authDomain: `${appName}.firebaseapp.com`,
   databaseURL: `https://${appName}.firebaseio.com`,
   projectId: appName,
   storageBucket: `${appName}.appspot.com`,
-  messagingSenderId: '842367541781'
+  messagingSenderId: '1010171927497'
 }
 
-firebase.initializeApp(fbConfig)
+window.firebase = firebase.initializeApp(fbConfig)
