974cc5d1ea50e770aea6af29f405d4603fe13be6
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-09-14 20:53:56 +0300

    add image example

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 //import HelloWorld from './components/hello-world'
-import Auth from './components/auth'
+//import Auth from './components/auth'
+import Event from './components/event'
+import data from './fixtures'
+const eventList = Object.entries(data.events).map(([ id, event ]) => ({ id, ...event }))
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-          <Auth/>
+          <Event event = {eventList[0]}/>
       </View>
     );
   }
--- mobile/components/event.js
+++ mobile/components/event.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, Text, Image } from 'react-native'
 
 class Event extends Component {
-    static propTypes = {
-
-    };
 
     render() {
+        const { title, when, url } = this.props.event
         return (
             <View>
+                <Image source={{ uri: 'http://lorempixel.com/400/200'}}
+                    style = {styles.image}
+                />
+                <Text>{title}</Text>
+                <Text>{when}</Text>
+                <Text>{url}</Text>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    image: {
+        width: 200,
+        height: 100
+    }
 })
 
 export default Event
