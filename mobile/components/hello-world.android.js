a7b7d65631e867d3e77d92683afd2bad00737b91
Roman Iakobchuk <kaboom.ua@gmail.com>
2018-09-14 20:11:16 +0300

    add hello-world

---
+++ mobile/components/hello-world.android.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+
+class HelloWorldAnroid extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>Hello Android</Text>
+            </View>
+        )
+    }
+}
+
+export default HelloWorldAnroid
---
+++ mobile/components/hello-world.ios.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+
+class HelloWorldIos extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <View>
+                <Text>Hello Ios</Text>
+            </View>
+        )
+    }
+}
+
+export default HelloWorldIos
