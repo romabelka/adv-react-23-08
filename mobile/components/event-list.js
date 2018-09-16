0af40a900f215aaa6fff0db2aca9ce3fd405709b
Leisan Kazberova <leisan.kh@gmail.com>
2018-09-16 21:45:30 +0300

    HT6.2

--- mobile/components/event-list.js
+++ mobile/components/event-list.js
@@ -1,20 +1,40 @@
-import React, {Component} from 'react'
-import {ScrollView} from 'react-native'
-import {EventListItem} from "./event-list-item";
+import React, { Component } from "react";
+import { SectionList, Text } from "react-native";
+import { EventListItem } from "./event-list-item";
 
 class EventList extends Component {
-    static propTypes = {
+  static propTypes = {};
 
-    };
-
-    render() {
-        return (
-            <ScrollView>
-                {this.props.events.map(event => <EventListItem key = {event.id} title={event.title} onClick={this.clickHandler(event)}/>)}
-            </ScrollView>
-        )
-    }
-    clickHandler = event => () => this.props.onSelect(event)
+  render() {
+    return (
+      <SectionList
+        renderItem={({ item }) => (
+          <EventListItem
+            key={item.id}
+            title={item.title}
+            onClick={this.clickHandler(item)}
+          />
+        )}
+        renderSectionHeader={({ section: { title, data } }) => (
+          <Text style={{ fontWeight: "bold" }}>
+            {title} ({data.length})
+          </Text>
+        )}
+        keyExtractor={item => item.id}
+        sections={groupData(this.props.events)}
+      />
+    );
+  }
+  clickHandler = event => () => this.props.onSelect(event);
 }
+const groupData = data =>
+  Object.values(
+    data.reduce((r, e) => {
+      let title = e.title[0];
+      if (!r[title]) r[title] = { title, data: [e] };
+      else r[title].data.push(e);
+      return r;
+    }, {})
+  );
 
-export default EventList
+export default EventList;
