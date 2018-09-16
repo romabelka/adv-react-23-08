129a4f79debf0ee40acbed3642a0bb852fc5d6dd
Alexey Germanov <lexx.vl@gmail.com>
2018-09-16 21:27:59 +0300

    Comple task ht6.2

--- mobile/App.js
+++ mobile/App.js
@@ -2,10 +2,34 @@ import React from "react";
 import { StyleSheet, Image, SafeAreaView, StatusBar } from "react-native";
 //import HelloWorld from './components/hello-world'
 //import Auth from './components/auth'
-import Event from './components/event'
-import EventList from './components/event-list'
-import data from './fixtures'
-const eventList = Object.entries(data.events).map(([ id, event ]) => ({ id, ...event }))
+import Event from "./components/event";
+import EventList from "./components/event-list";
+import data from "./fixtures";
+
+const eventList = Object.entries(data.events).map(([id, event]) => ({
+  id,
+  ...event
+}));
+
+const sectionedEventList = eventList
+  .reduce((acc, item, i) => {
+    const letter = item.title.slice(0, 1).toUpperCase();
+    const index = acc.findIndex(
+      ({ title }) => title.slice(0, 1).toUpperCase() === letter
+    );
+
+    if (index < 0) return [...acc, { title: letter, data: [item] }];
+
+    const section = acc[index];
+
+    return [
+      ...acc.slice(0, index),
+      { ...section, data: [...section.data, item] },
+      ...acc.slice(index + 1)
+    ];
+  }, [])
+  .sort(({ title: title1 }, { title: title2 }) => title1 > title2)
+  .map(({ title, data }) => ({ title: `${title} (${data.length})`, data }));
 
 export default class App extends React.Component {
   render() {
@@ -15,8 +39,8 @@ export default class App extends React.Component {
           source={require("./assets/logo.png")}
           style={styles.image}
           resizeMode="contain"
-          />
-        <EventList events={eventList} />
+        />
+        <EventList events={sectionedEventList} />
       </SafeAreaView>
     );
   }
@@ -27,8 +51,8 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     flex: 1
   },
-    image: {
+  image: {
     width: "100%",
-      height: 100
-    }
+    height: 100
+  }
 });
