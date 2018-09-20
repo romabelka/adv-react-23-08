3bd3533557d89304d0e3adf9ae944fd4f1f9d29d
Victor Zagorodny <post.vittorius@gmail.com>
2018-09-20 20:24:27 +0300

    feat: add firebase sign in

--- mobile/components/screens/auth.js
+++ mobile/components/screens/auth.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
+import { ActivityIndicator, StyleSheet } from 'react-native'
 import Auth from '../auth'
+import { autorun } from 'mobx'
+import { observer, inject } from 'mobx-react'
 
+@inject('auth')
+@observer
 class AuthScreen extends Component {
-    static propTypes = {
+  static propTypes = {}
+  authenticatedDisposer = null
 
+  onSuccessfulSignIn = () => {
+    this.props.navigation.navigate('eventList')
+  }
+
+  componentDidMount() {
+    this.authenticatedDisposer = autorun(() => {
+      if (this.props.auth.authenticated) {
+        this.onSuccessfulSignIn()
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.authenticatedDisposer()
+  }
+
+  handleSignIn = () => this.props.auth.signIn()
+
+  render() {
+    if (this.props.auth.loadingNewState) {
+      return <ActivityIndicator size="large" style={styles.activity} />
+    } else {
+      return <Auth onSignIn={this.handleSignIn} />
     }
-
-    render() {
-        return <Auth onSignIn = {this.handleSignIn}/>
-    }
-
-    handleSignIn = () => this.props.navigation.navigate('eventList')
+  }
 }
+
+const styles = StyleSheet.create({
+  activity: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 
 export default AuthScreen
--- mobile/stores/auth.js
+++ mobile/stores/auth.js
@@ -1,20 +1,54 @@
-import {observable, action, computed} from 'mobx'
-import {validate} from 'email-validator'
+import { observable, action, computed } from 'mobx'
+import { validate } from 'email-validator'
+import firebase from 'firebase/app'
 
 class AuthStore {
-    @observable email = ''
-    @observable password = ''
+  @observable
+  email = ''
+  @observable
+  password = ''
+  @observable
+  loadingNewState = false
+  @observable
+  user = null
+  onAuthStateChangedDisposer = null
 
-    @computed get isValidEmail() {
-        return validate(this.email)
-    }
+  @computed
+  get isValidEmail() {
+    return validate(this.email)
+  }
 
-    @action changeEmail = email => {
-        this.email = email
-        this.email = ''
-        this.email = email
+  @computed
+  get authenticated() {
+    return !!this.user
+  }
+
+  @action
+  changeEmail = (email) => {
+    this.email = email
+    this.email = ''
+    this.email = email
+  }
+
+  @action
+  changePassword = (password) => (this.password = password)
+
+  @action
+  onAuthStateChanged = (user) => {
+    this.loadingNewState = false
+    this.user = user
+  }
+
+  @action
+  signIn = () => {
+    this.loadingNewState = true
+    if (!this.onAuthStateChangedDisposer) {
+      this.onAuthStateChangedDisposer = firebase
+        .auth()
+        .onAuthStateChanged(this.onAuthStateChanged)
     }
-    @action changePassword = password => this.password = password
+    firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+  }
 }
 
 export default AuthStore
