8ad62c78bde1e0830d780e588a19b5f453b97d6d
Victor Zagorodny <post.vittorius@gmail.com>
2018-09-20 20:55:40 +0300

    feat: disable back nav after sign in

--- mobile/components/app-navigator.js
+++ mobile/components/app-navigator.js
@@ -1,19 +1,22 @@
-import {createStackNavigator} from 'react-navigation'
+import { createStackNavigator } from 'react-navigation'
 import AuthScreen from './screens/auth'
 import EventScreen from './screens/event'
 import EventListScreen from './screens/event-list'
 
 export default createStackNavigator({
-    auth: {
-        screen: AuthScreen,
-        navigationOptions: {
-            title: 'Sign In'
-        }
-    },
-    event: {
-        screen: EventScreen
-    },
-    eventList: {
-        screen: EventListScreen
+  auth: {
+    screen: AuthScreen,
+    navigationOptions: {
+      title: 'Sign In'
     }
+  },
+  event: {
+    screen: EventScreen
+  },
+  eventList: {
+    screen: EventListScreen,
+    navigationOptions: {
+      headerLeft: null
+    }
+  }
 })
