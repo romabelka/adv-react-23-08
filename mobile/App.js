c00ecb8bade9c389a6d007d287f703a872a977b5
Alexey Germanov <lexx.vl@gmail.com>
2018-09-21 00:19:39 +0300

    add firebase in mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,3 +1,5 @@
+import './config'
+
 import React from 'react'
 import { configure } from 'mobx'
 import { Provider } from 'mobx-react'
---
+++ mobile/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+export const appName = 'adv-react-6fc9a'
+
+const fbConfig = {
+  apiKey: 'AIzaSyAJiE6RMMX1iCLCY9YbRAhn6i5wp_4k1QU',
+  authDomain: `${appName}.firebaseapp.com`,
+  databaseURL: `https://${appName}.firebaseio.com`,
+  projectId: appName,
+  storageBucket: `${appName}.appspot.com`,
+  messagingSenderId: '398253872891'
+}
+
+firebase.initializeApp(fbConfig)
