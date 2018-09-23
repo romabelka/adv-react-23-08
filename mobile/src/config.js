135b04fda0744de3d1729324788756d6327307d9
Victor Zagorodny <post.vittorius@gmail.com>
2018-09-23 20:39:08 +0300

    chore: update config

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/database'
+import 'firebase/storage'
 
-export const appName = 'adv-react-23-08'
+export const appName = 'adv-react-23-08-vittorius'
 
 const fbConfig = {
-    apiKey: 'AIzaSyAlWpSCiLQOATDM2O869VEYBfT3kuDuxc0',
-    authDomain: `${appName}.firebaseapp.com`,
-    databaseURL: `https://${appName}.firebaseio.com`,
-    projectId: appName,
-    storageBucket: `${appName}.appspot.com`,
-    messagingSenderId: '842367541781'
+  apiKey: 'AIzaSyCgw2wKSWCuWBy3ZSqvn5O3Q7mq1wf21ug',
+  authDomain: `${appName}.firebaseapp.com`,
+  databaseURL: `https://${appName}.firebaseio.com`,
+  projectId: appName,
+  storageBucket: `${appName}.appspot.com`,
+  messagingSenderId: '999406358227'
 }
 
 firebase.initializeApp(fbConfig)
